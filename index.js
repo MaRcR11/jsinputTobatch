89b4db36067b310e14f5700303282b3657553c37
MaRcR11 <91952791+MaRcR11@users.noreply.github.com>
2022-04-19 14:19:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Give someting for .txt?', input => {
+    console.log(`Your input: ${input}`);
+    readline.close();
+    fs.writeFile('YOUR ABSOLUTE PATH', input, err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+    })
+});
+
+
